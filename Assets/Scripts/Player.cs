7e19b73001f3ead9b01d18fbf005d571a0d7a613
Zh00c3q <maciej.a.zak@gmail.com>
2020-05-01 12:24:59 +0200

    add standard assets and create Player script with horizontal movement controls

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour
+{
+
+    Rigidbody2D myRigidBody;
+    [SerializeField] float Speed = 3f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidBody = GetComponent<Rigidbody2D>();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Run();
+    }
+
+    private void Run()
+    {
+        Animation.Running = true;
+        float controlThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        Vector2 playerVelocity = new Vector2(controlThrow * Speed, myRigidBody.velocity.y);
+        myRigidBody.velocity = playerVelocity;
+    }
+}
