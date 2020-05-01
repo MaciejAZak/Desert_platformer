4ef5556b8ba9fda4a5f5955a35586bb02a782caa
Zh00c3q <maciej.a.zak@gmail.com>
2020-05-01 17:01:50 +0200

    add FlipSprite method to Player script

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -19,13 +19,22 @@ public class Player : MonoBehaviour
     void Update()
     {
         Run();
+        FlipSprite();
     }
 
     private void Run()
     {
-        Animation.Running = true;
         float controlThrow = CrossPlatformInputManager.GetAxis("Horizontal");
         Vector2 playerVelocity = new Vector2(controlThrow * Speed, myRigidBody.velocity.y);
         myRigidBody.velocity = playerVelocity;
+    }
+
+    private void FlipSprite()
+    {
+        bool playerHasHorizontalSpeed = Mathf.Abs(myRigidBody.velocity.x) > Mathf.Epsilon;
+        if (playerHasHorizontalSpeed)
+        {
+            transform.localScale = new Vector2(Mathf.Sign(myRigidBody.velocity.x), 1f);
+        }
     }
 }
