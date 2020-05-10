936d85384bb2d0f4fab6d7b519f5a50744cd2915
Zh00c3q <maciej.a.zak@gmail.com>
2020-05-10 19:05:53 +0200

    Climb ladder tweaks

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,18 +9,23 @@ public class Player : MonoBehaviour
     [SerializeField] float ClimbSpeed = 4f;
     [SerializeField] float jumpSpeed = 15f;
     bool isAlive = true;
+    bool climbingRightNow = false;
 
+    Transform myTransform;
     Rigidbody2D myRigidBody;
     Animator myAnimator;
     Collider2D myCollider2D;
-
+    float gravityAtStart;
 
     // Start is called before the first frame update
     void Start()
     {
+        myTransform = GetComponent<Transform>();
         myRigidBody = GetComponent<Rigidbody2D>();
         myAnimator = GetComponent<Animator>();
         myCollider2D = GetComponent<Collider2D>();
+        gravityAtStart = myRigidBody.gravityScale;
+        climbingRightNow = false;
     }
 
     // Update is called once per frame
@@ -30,6 +35,8 @@ public class Player : MonoBehaviour
         FlipSprite();
         JumpValidation();
         Climb();
+        //ShowPosition();
+        IsClimbing();
     }
 
     private void Run()
@@ -39,7 +46,7 @@ public class Player : MonoBehaviour
         if (Input.GetKey("left shift"))
         {
             Debug.Log("Shift");
-            myRigidBody.velocity = new Vector2 (controlThrow * Speed * 1.5f, myRigidBody.velocity.y);
+            myRigidBody.velocity = new Vector2(controlThrow * Speed * 1.5f, myRigidBody.velocity.y);
         }
         else
         {
@@ -71,30 +78,71 @@ public class Player : MonoBehaviour
         if (CrossPlatformInputManager.GetButtonDown("Jump"))
         {
             Debug.Log("Jumped");
+            ReleaseLadder();
             Vector2 jumpVelocityToAdd = new Vector2(0f, jumpSpeed);
             myRigidBody.velocity += jumpVelocityToAdd;
+            
         }
     }
 
     private void Climb()
+
+        //TODO: STOP CLIMBING WHEN YOU REACH TOP OF A LADDER (because currently you fall down once you stop colliding with a ladder)
+        //TODO: STOP MOVING LEFT AND RIGHT WHILE ON A LADDER
+        //TODO: AUTOMATICALY RELEASE THE LADDER WHEN YOU REACH THE BOTTOM
     {
-        if (myCollider2D.IsTouchingLayers(LayerMask.GetMask("Ladders")))
+        float controlThrow = CrossPlatformInputManager.GetAxis("Vertical");
+        if (myCollider2D.IsTouchingLayers(LayerMask.GetMask("Ladders")) && controlThrow != 0)
         {
-            Debug.Log("Touching ladder");
-            float controlThrow = CrossPlatformInputManager.GetAxis("Vertical");
-            if (controlThrow !=0)
+            climbingRightNow = true;
+        }
+
+        if (!myCollider2D.IsTouchingLayers(LayerMask.GetMask("Ladders")))
+        {
+            Debug.Log("Not touching ladder");
+            ReleaseLadder();
+
+        }
+
+        if (climbingRightNow == true)
+        {
+            //Debug.Log("Touching ladder");
+            if (controlThrow != 0)
             {
+                myRigidBody.gravityScale = 0f;
                 Vector2 playerVelocity = new Vector2(myRigidBody.velocity.x, controlThrow * ClimbSpeed);
                 myRigidBody.velocity = playerVelocity;
                 bool playerHasVerticalSpeed = Mathf.Abs(myRigidBody.velocity.y) > Mathf.Epsilon;
                 myAnimator.SetBool("Climbing", playerHasVerticalSpeed);
             }
+            else if (controlThrow == 0)
+            {
+                myRigidBody.gravityScale = 0f;
+                Vector2 playerVelocity = new Vector2(myRigidBody.velocity.x, controlThrow * ClimbSpeed);
+                myRigidBody.velocity = playerVelocity;
+                return;
+            }
+
         }
-        else
-        {
-            Debug.Log("Not touching ladder");
-            myAnimator.SetBool("Climbing", false);
-        }
+    }
+
+    private void ReleaseLadder()
+    {
+        climbingRightNow = false;
+        myRigidBody.gravityScale = gravityAtStart;
+        myAnimator.SetBool("Climbing", false);
+    }
+
+    private void ShowPosition()
+    {
+
+        Debug.Log(myTransform.transform.position);
+    }
+
+    private void IsClimbing()
+    {
+
+        Debug.Log(climbingRightNow);
     }
 
     private void FlipSprite()
