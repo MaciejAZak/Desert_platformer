8fc1c6999688a739c060fa4fdb04aa53f9c7a61d
Zh00c3q <maciej.a.zak@gmail.com>
2020-05-31 12:56:38 +0200

    add game flow from main menu to win screen

---
+++ Assets/Scripts/ExitLevel.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ExitLevel : MonoBehaviour
+{
+
+    BoxCollider2D doorCollider;
+    [SerializeField] float LoadLevelDelay = 3f;
+    GameObject label;
+
+    private void Start()
+    {
+        doorCollider = GetComponent<BoxCollider2D>();
+        // label = GetComponent<>//Get children
+    }
+
+    private void Update()
+    {
+        if (doorCollider.IsTouchingLayers(LayerMask.GetMask("Player")))
+        {
+            //SHOW WHICH BUTTON TO CLICK
+            Debug.Log("Touching door");
+            if (Input.GetKeyDown("return"))
+            {
+                StartCoroutine(LoadNextLevel());
+                //TODO: Fireworks + music + "Level complete"
+            }
+        }
+    }
+
+    IEnumerator LoadNextLevel()
+    {
+        yield return new WaitForSecondsRealtime(LoadLevelDelay);
+        var currentLevelIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentLevelIndex + 1);
+    }
+}
---
+++ Assets/Scripts/MenuStart.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuStart : MonoBehaviour
+{
+    public void StartFirstLevel()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
