715b6e949ca8cda610d64b3d9ac4cebe49401465
Zh00c3q <maciej.a.zak@gmail.com>
2020-06-02 18:34:16 +0200

    add SFX to orange pickup

--- Assets/Pickup.cs
+++ Assets/Pickup.cs
@@ -4,12 +4,15 @@ using UnityEngine;
 
 public class Pickup : MonoBehaviour
 {
+    [SerializeField] AudioClip pickupSFX;
     BoxCollider2D myCollider2D;
     Animator myAnimator;
+    
 
     public void OnTriggerEnter2D(Collider2D collision)
     {
         Debug.Log("Pickup!");
+        AudioSource.PlayClipAtPoint(pickupSFX, FindObjectOfType<Camera>().transform.position);
         myAnimator = GetComponent<Animator>();
         myAnimator.SetTrigger("picked");
         StartCoroutine(destroyPickup());
