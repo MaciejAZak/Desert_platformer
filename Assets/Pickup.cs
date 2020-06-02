8f83f650de3a036eea8e25dfc3611aa8faa55701
Zh00c3q <maciej.a.zak@gmail.com>
2020-06-02 17:14:27 +0200

    add orange pickup prefab

---
+++ Assets/Pickup.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour
+{
+    BoxCollider2D myCollider2D;
+    Animator myAnimator;
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Pickup!");
+        myAnimator = GetComponent<Animator>();
+        myAnimator.SetTrigger("picked");
+        StartCoroutine(destroyPickup());
+    }
+
+    IEnumerator destroyPickup()
+    {
+        yield return new WaitForSecondsRealtime(3f);
+
+        Destroy(gameObject);
+    }
+}
--- Assets/Scripts/MenuStart.cs
+++ Assets/Scripts/MenuStart.cs
@@ -5,16 +5,28 @@ using UnityEngine.SceneManagement;
 
 public class MenuStart : MonoBehaviour
 {
+
     public void StartFirstLevel()
     {
+        ResetGameSession();
         SceneManager.LoadScene(1);
     }
     public void BackToMenu()
     {
+        ResetGameSession();
         SceneManager.LoadScene(0);
     }
     public void QuitGame()
     {
         Application.Quit();
+    }
+
+    public void ResetGameSession()
+    {
+        if (FindObjectOfType<GameSession>() != null)
+        {
+            var gameSession = FindObjectOfType<GameSession>().gameObject;
+            Destroy(gameSession);
+        }
     }
 }
