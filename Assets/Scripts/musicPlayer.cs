228dffe64b8629773e866340e49c7cd6b27258bf
Zh00c3q <maciej.a.zak@gmail.com>
2020-06-10 16:16:53 +0200

    add MusicPlayer

---
+++ Assets/Scripts/musicPlayer.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class musicPlayer : MonoBehaviour
+{
+
+    AudioSource audioSource;
+
+    // Start is called before the first frame update
+    private void Awake()
+    {
+
+        {
+            int numScenePersist = FindObjectsOfType<musicPlayer>().Length;
+            if (numScenePersist > 1)
+            {
+                Destroy(gameObject);
+                Debug.Log("ScenePersist has been destoyed due to Singleton");
+            }
+            else
+            {
+                DontDestroyOnLoad(gameObject);
+            }
+        }
+
+    }
+    void Start()
+    {
+        DontDestroyOnLoad(this);
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
